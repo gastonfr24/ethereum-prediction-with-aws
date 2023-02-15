ccd4a624357c20fd32e6a252ad91b24b8e2dd771
Gaston Franco <102929225+gastonfr24@users.noreply.github.com>
2023-02-14 21:43:30 -0300

    exportar modelo

--- train_predict-SageMakerScript.py
+++ train_predict-SageMakerScript.py
@@ -5,27 +5,30 @@ import requests
 import pandas as pd
 import numpy as np
 
-from datetime import datetime
+from datetime import datetime, date
 import time
 
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 from sklearn.preprocessing import MinMaxScaler
 
-
 # Función para obtener los precios de Ethereum de la API
-def get_eth_price(date = '2022-12-31'):
-    
-    url = 'https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from=1392577232&to=1675911600'
+def get_eth_price(date):
+    test= 1675220400
+    today_datetime = datetime.combine(date, datetime.min.time())
+    timestamp = int(today_datetime.timestamp())
+    url = f'https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from=1392577232&to={timestamp}'
     response = requests.get(url)
     data = response.json()
     prices = data['prices']
     df = pd.DataFrame(prices, columns=['timestamp', 'price'])
     df['date'] = df['timestamp'].apply(lambda x: datetime.fromtimestamp(x/1000))
     df = df.drop('timestamp', axis=1)
+    df = df.iloc[:-1,:]
     return df
 
-
+# Función para la normalización y escalado + preproceso para entrada a la red LSTM
 def preprocessing_data(training_set):
     training_set = training_set.iloc[:,0:1].values
     # Escalado de variables
@@ -43,8 +46,11 @@ def preprocessing_data(training_set):
     X_train, y_train = np.array(X_train), np.array(y_train)
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
     return X_train, y_train, sc
-    
+
+
+# Creación y entrenamiento del modelo
 def training_model(X_train, y_train):
+    tf.keras.backend.set_image_data_format("channels_last")
     model = Sequential()
     model.add(LSTM(64, return_sequences = True, input_shape = (X_train.shape[1],1)))
     model.add(Dropout(0.25))
@@ -56,7 +62,7 @@ def training_model(X_train, y_train):
     model.add(Dropout(0.25))
     model.add(Dense(1))
     model.compile(optimizer='Nadam', loss='mean_squared_error')
-    model.fit(X_train, y_train, epochs=50, batch_size=32)
+    model.fit(X_train, y_train, epochs=32, batch_size=32)
     return model
 
 
@@ -82,15 +88,17 @@ def predict(model, data, sc):
      return y_pred
 
 
-
 # Main loop
-today = datetime.now().strftime("%Y-%m-%d")
-data = get_eth_price(date = today)
-X_train, y_train,sc = preprocessing_data(data)
-model = training_model(X_train, y_train)
-prediction = predict(model, data, sc)
-            
-
+try:
+    today = date.today()
+    data = get_eth_price(date = today)
+    X_train, y_train,sc = preprocessing_data(data)
+    model = training_model(X_train, y_train)
+    prediction = predict(model, data, sc)
+except Exception as e:
+    print(e)
+    
+    
 # Exportar predicción
 import os
 import boto3
@@ -116,7 +124,7 @@ session = boto3.Session(
 s3 = session.resource("s3")
 
 # Nombre del bucket y la carpeta donde se guardará la predicción
-bucket_name = STORAGE_BUCKET_NAME
+bucket_name = 'gfranco'
 folder_name = "predicts"
 file_name = "prediction-{}.json".format(datetime.now().month)
 
@@ -125,8 +133,3 @@ file_object = s3.Object(bucket_name, f"{folder_name}/{file_name}")
 
 # Escribe la predicción en el archivo
 file_object.put(Body=prediction_json.encode("utf-8"))
-
-
-
-
-
