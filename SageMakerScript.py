3111182e1473851f333a9a5020b9eb62797c72c3
Gaston Franco <102929225+gastonfr24@users.noreply.github.com>
2023-02-17 00:31:43 -0300

    best model

--- SageMakerScript.py
+++ SageMakerScript.py
@@ -1,11 +1,9 @@
-#Created on Thu Feb  9 07:33:12 2023
-#@author: Gaston Franco
 
 import requests
 import pandas as pd
 import numpy as np
 
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 import time
 
 import tensorflow as tf
@@ -13,12 +11,17 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 from sklearn.preprocessing import MinMaxScaler
 
+import os
+import boto3
+import json
+
 # Función para obtener los precios de Ethereum de la API
 def get_eth_price(date):
     test= 1675220400
     today_datetime = datetime.combine(date, datetime.min.time())
     timestamp = int(today_datetime.timestamp())
-    url = f'https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from=1392577232&to={timestamp}'
+    
+    url = f'https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from={1664593200}&to={timestamp}'
     response = requests.get(url)
     data = response.json()
     prices = data['prices']
@@ -28,23 +31,24 @@ def get_eth_price(date):
     df = df.iloc[:-1,:]
     return df
 
-# Función para la normalización y escalado + preproceso para entrada a la red LSTM
+
 def preprocessing_data(training_set):
     training_set = training_set.iloc[:,0:1].values
     # Escalado de variables
     sc = MinMaxScaler()
     
     training_set_scaled = sc.fit_transform(training_set)
+    
     X_train = []
     y_train = []
     
-    for i in range(60,len(training_set)):
-        
-        X_train.append(training_set_scaled[i-60:i, 0])
+    for i in range(15, len(training_set_scaled)):
+        X_train.append(training_set_scaled[i-15:i, 0])
         y_train.append(training_set_scaled[i, 0])
         
     X_train, y_train = np.array(X_train), np.array(y_train)
     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+    
     return X_train, y_train, sc
 
 
@@ -52,84 +56,75 @@ def preprocessing_data(training_set):
 def training_model(X_train, y_train):
     tf.keras.backend.set_image_data_format("channels_last")
     model = Sequential()
-    model.add(LSTM(64, return_sequences = True, input_shape = (X_train.shape[1],1)))
-    model.add(Dropout(0.25))
-    model.add(LSTM(64, return_sequences = True))
-    model.add(Dropout(0.25))
-    model.add(LSTM(64, return_sequences = True))
-    model.add(Dropout(0.25))
-    model.add(LSTM(64, return_sequences = False))
-    model.add(Dropout(0.25))
+    model.add(LSTM(50, return_sequences = True, input_shape = (X_train.shape[1],1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(50, return_sequences = True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(50, return_sequences = True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(50, return_sequences = False))
+    model.add(Dropout(0.2))
     model.add(Dense(1))
-    model.compile(optimizer='Nadam', loss='mean_squared_error')
-    model.fit(X_train, y_train, epochs=32, batch_size=32)
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.005), loss='mean_squared_error')
+    model.fit(X_train, y_train, epochs=1000, batch_size=32)
     return model
 
-
-# Función para hacer la predicción
-def predict(model, data, sc):
-     dataset = data['price']
-     inputs = dataset[len(dataset)-90:].values
-     inputs = inputs.reshape(-1,1)
-     # Escalado de variables
-     
-     inputs = sc.transform(inputs)
-     X_train = []
-     
-     for i in range(60,90):
-         X_train.append(inputs[i-60:i, 0])
-
-         
-     X_train= np.array(X_train)
-     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-     
-     # Haciendo predicción
-     y_pred = sc.inverse_transform(model.predict(X_train))
-     return y_pred
+def predict_prices_for_month(training_set, sc, model, days=30):
+    all_predicts = []
+    last_days = training_set[-15:]
+    for i in range(days):
+        last_days_scaled = sc.transform(last_days)
+        X_test = np.array([last_days_scaled])
+        predicted_price_scaled = model.predict(X_test)
+        predicted_price = sc.inverse_transform(predicted_price_scaled)
+        all_predicts.append(predicted_price[0,0])
+        last_days = np.concatenate([last_days, predicted_price], axis=0)[-15:]
+    return all_predicts
 
 
-# Main loop
-try:
-    today = date.today()
-    data = get_eth_price(date = today)
-    X_train, y_train,sc = preprocessing_data(data)
-    model = training_model(X_train, y_train)
-    prediction = predict(model, data, sc)
-except Exception as e:
-    print(e)
-    
-    
-# Exportar predicción
-import os
-import boto3
-import json
-
-# Prediccion a formato json
-prediction_dict = {i+1: float(j[0]) for i,j in enumerate(prediction)}
-prediction_json = json.dumps(prediction_dict)
-
-
-ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-
-
-# Crea una sesión de AWS con las credenciales
-session = boto3.Session(
-    aws_access_key_id=ACCESS_KEY_ID,
-    aws_secret_access_key=SECRET_ACCESS_KEY
-)
-
-# Crea un objeto S3
-s3 = session.resource("s3")
-
-# Nombre del bucket y la carpeta donde se guardará la predicción
-bucket_name = 'gfranco'
-folder_name = "predicts"
-file_name = "prediction-{}.json".format(datetime.now().month)
+def json_to_aws(day_predictions):
+    try:
+        prediction_json = json.dumps(day_predictions)
+        
+        # Crea una sesión de AWS con las credenciales
+        session = boto3.Session(
+            aws_access_key_id='AKIA6RESZTAZXQQBLU2D',
+            aws_secret_access_key='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'
+        )
+        
+        # Crea un objeto S3
+        s3 = session.resource("s3")
+        
+        # Nombre del bucket y la carpeta donde se guardará la predicción
+        bucket_name = 'gfranco'
+        folder_name = "predicts"
+        file_name = "prediction-{}.json".format(datetime.now().month)
+        
+        
+        file_object = s3.Object(bucket_name, f"{folder_name}/{file_name}")
+        
+        # Escribe la predicción en el archivo
+        file_object.put(Body=prediction_json.encode("utf-8"))
+        return 'se ha enviado la predicciones'
+    except Exception as e:
+        return str(e)
 
 
-file_object = s3.Object(bucket_name, f"{folder_name}/{file_name}")
 
-# Escribe la predicción en el archivo
-file_object.put(Body=prediction_json.encode("utf-8"))
+
+today = date.today()
+training_set = get_eth_price(today)
+X_train, y_train, sc = preprocessing_data(training_set)
+model = training_model(X_train, y_train)
+training_set_pred = training_set.iloc[:,0:1].values
+month_predict = predict_prices_for_month(training_set_pred, sc, model)
+day_predictions = {i+1: float(prediction) for i, prediction in enumerate(month_predict)}
+json_to_aws(day_predictions)
+
+
+
+import matplotlib.pyplot as plt
+df = pd.DataFrame(month_predict, columns=['0'])
+# Graficar los datos
+df.plot()
+plt.show()
